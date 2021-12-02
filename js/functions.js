ccdb04f93fc8f1dd0747725303c7e1112800b85d
Ihor <stivok1@mail.ru>
2021-12-02 14:03:56 +0300

    добави и реализовал кнопку Delet

--- js/functions.js
+++ js/functions.js
@@ -20,9 +20,9 @@ function createMenu() {                                  // функция ко�
     document.querySelector('.btn-wrapper').append(productTitle, carsTitle, userTitle)
 }
 
-function createDivElement(arrElement, arr) {            // функция которая создаёт отдельный контейнер для каждого еллемента списка. В ней есть пока титул и список характеристик, позже будут кнопки
+function createDivElement(arrElement, arrElements) {            // функция которая создаёт отдельный контейнер для каждого еллемента списка. В ней есть пока титул и список характеристик, позже будут кнопки
     const div = createDomElement('div', {class: 'List_element', data_id: arrElement.id})
-    const title = createDomElement('h4', {}, (arr.indexOf(arrElement) + 1) + '.' + arrElement.name)
+    const title = createDomElement('h4', {}, (arrElements.indexOf(arrElement) + 1) + '.' + arrElement.name)
     const ul = createDomElement('ul', {class:'information'})
 
     for (let key in arrElement) {
@@ -38,10 +38,41 @@ function createDivElement(arrElement, arr) {            // функция кот
     buttonViev.addEventListener('click', event => showUl(event))
 
     const buttonEdit = createDomElement('button', {}, 'Edit')
+
     const buttonDelet = createDomElement('button', {}, 'Delet')
+    buttonDelet.addEventListener('click', event => deletElement(event, div, arrElements))
 
     div.append(title,buttonViev, buttonEdit, buttonDelet, ul)
     document.querySelector('main').appendChild(div)
+}
+
+function deletElement(event, divWithId, arrElements) {                        //удаление из списка: пользователя, товара или машины, я думаю эту функцию можно лучше сделать, но это лучше вдвоём посмотреть и решить 
+    const deletElement = divWithId.getAttribute('data_id')
+    const div = createDomElement('div', {class: 'confirmDelet'})
+    div.classList.toggle('show')
+    const divYesNo = createDomElement('div', {class: 'yes_or_no'})
+    const span = createDomElement('span', {}, 'Are you sure you want to delete it?')
+    const buttonYes = createDomElement('button', {}, 'Yes')
+    buttonYes.addEventListener('click', () => {
+
+        for (let i = 0; i < arrElements.length; i++) {
+            if (deletElement == arrElements[i].id) {
+                arrElements.splice(arrElements.indexOf(arrElements[i]), 1)
+                break
+            }
+        }
+
+        document.querySelector('main').removeChild(event.target.parentNode)
+        document.body.removeChild(document.querySelector('.confirmDelet'))
+    })
+
+    const buttonNo = createDomElement('button', {}, 'No')
+    buttonNo.addEventListener('click', () => document.body.removeChild(document.querySelector('.confirmDelet')))
+
+
+    divYesNo.append(span, buttonYes, buttonNo)
+    div.append(divYesNo)
+    document.body.appendChild(div)
 }
 
 function showUl(event) {                              // показывает и скрывает сисок характеристик при клике
@@ -49,10 +80,10 @@ function showUl(event) {                              // показывает и
     ul.classList.toggle('show')
 }
 
-function createListElements(arr) {                   // функция которая затирает старый список и создаёт новый 
+function createListElements(arrElements) {                   // функция которая затирает старый список и создаёт новый 
     document.querySelector('main').innerHTML =''
 
-    for(let i = 0; i<arr.length; i++) {
-        createDivElement(arr[i], arr)
+    for(let i = 0; i<arrElements.length; i++) {
+        createDivElement(arrElements[i], arrElements)
     }
 }
